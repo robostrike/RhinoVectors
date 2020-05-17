f6d2407b0dea6e850f82cc2bee826656c0e6c692
Martin Lui <robostrike.me@gmail.com>
2020-05-17 11:59:09 -0400

    Update randomGen.py

--- randomGen.py
+++ randomGen.py
@@ -1,7 +1,8 @@
 """
 This code is to develop and create a random variable assessibility for use in Rhinoscript space.
 Vers. 1.0:
-    Uses Linear Congruential Generator to compute the random values
+    Uses Linear Congruential Generator to compute the random values (and control them)
+    Map function to remap domains for random values
 
 Developed by Robostrike
 Started on April 28, 2020
@@ -23,6 +24,10 @@ class randVal:
         self.x1 = seed
         self.mo = val
     
+    def value(self):
+        #returns the domain value of the random class object
+        return self.mo
+    
     def next(self):
         #generates the next value output
         #eg. rand.next() --> will generate the next tabled value
@@ -30,3 +35,9 @@ class randVal:
         self.x1 = self.xn
         
         return int(self.xn%self.mo)
+
+def map(domain,rand):
+    #domain is the range from 0 to domain that the map will correlate to
+    #rand is the class object of randVal
+    
+    return domain*rand.next()/rand.value()
